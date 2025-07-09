03f579d9d80fc47f3331c3b763615a68f2e46891
Diego Espindola <diego.espindola@gmail.com>
2025-07-09 01:39:06 -0400

    coverage test

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Agrega la carpeta src al path para que Python pueda encontrar src.jsSecrets
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
